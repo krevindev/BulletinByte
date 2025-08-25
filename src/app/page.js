2257f0db80c5c09b51e65278877b4c65e0e0de66
krevindev <alimpuangonkylerevin@gmail.com>
2025-08-26 01:39:37 +0800

    Removed the navbar

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div className="h-full">
       <div className="w-ful min-h-[80%] bg-red-200">
-        <h1>Hello</h1>
       </div>
     </div>
   );
