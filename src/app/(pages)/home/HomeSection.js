3daafff11be5a827b31b95983dc12e021173a007
krevindev <alimpuangonkylerevin@gmail.com>
2025-08-26 03:25:50 +0800

    finished the homesection template

--- src/app/(pages)/home/HomeSection.js
+++ src/app/(pages)/home/HomeSection.js
@@ -2,17 +2,18 @@ import React from "react";
 
 const HomeSection = ({ title }) => {
   return (
-    <div className="bg-black w-full h-fit min-h-[300px]">
+    <div className="w-full h-fit min-h-[200px] py-5">
       {/* Title/Header */}
-      <div className="">
-        <div className="bg-yellow-500">
-          <h1>{title}</h1>
+      <div className="flex justify-center items-center">
+        <div className="bg-yellow-400 px-7 py-1 w-fit min-w-fit rounded-md">
+          <h1 className="text-black font-bold uppercase tracking-wider">{title}</h1>
         </div>
+        <div className="h-[1px] w-[90%] bg-[rgb(51,52,66)] mx-5" />
       </div>
 
       {/* Contents */}
-      <div>
-        <h5>SLDJF:</h5>
+      <div className="bg-red-200 w-full">
+        <h5>Placeholder</h5>
       </div>
     </div>
   );
