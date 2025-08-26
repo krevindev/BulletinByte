5fe9223090df63254797e65234337e0a0d8028f5
krevindev <alimpuangonkylerevin@gmail.com>
2025-08-26 16:09:07 +0800

    modified cors for local network testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: [
+      "http://192.168.1.4:3000", // your LAN IP
+      "http://localhost:3000",   // local dev
+    ],
+  },
+};
 
 export default nextConfig;
